0b47f228b723343e9f318f93c8db15a82f03e7b5
jimvelasco <jim.velasco@gmail.com>
2026-01-04 21:47:59 -0700

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,8 +5,8 @@ dotenv.config();
 const connectDB = async () => {
   try {
      console.log("MongoDB " + process.env.VITE_MONGO_URI);  
-     
-    await mongoose.connect(import.meta.env.VITE_MONGO_URI);
+
+    await mongoose.connect(process.env.VITE_MONGO_URI);
     console.log("MongoDB connected and running");
   } catch (error) {
     console.error(error);
