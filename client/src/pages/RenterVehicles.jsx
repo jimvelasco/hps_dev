1d33e8703482ae5332a8caadc4867f20b3e59540
jimvelasco <jim.velasco@gmail.com>
2026-01-09 21:30:57 -0700

    Update RenterVehicles.jsx

--- client/src/pages/RenterVehicles.jsx
+++ client/src/pages/RenterVehicles.jsx
@@ -215,7 +215,7 @@ export default function RenterVehicles() {
       <div className="page-content">
 
         {/* <div className="flexLayout"> */}
-                  <div className="flexLayout" style={{width:"380px",alignItems:"center",margin:"auto",marginBottom:"20px",justifyContent:"space-between"}}>
+                  <div className="flexLayout" style={{width:"360px",alignItems:"center",margin:"auto",marginBottom:"20px",justifyContent:"space-between"}}>
 
           <div className="page-label">
             <label className="input-label">
@@ -273,7 +273,7 @@ export default function RenterVehicles() {
         )}
         <br />
         {/* <div className="standardtitlebar-small"> */}
-           <div className="flexLayout" style={{width:"380px",alignItems:"center",margin:"auto",marginBottom:"20px",justifyContent:"center"}}>
+           <div className="flexLayout" style={{width:"360px",alignItems:"center",margin:"auto",marginBottom:"20px",justifyContent:"center"}}>
              <div className="page-label">
             <label className="input-label">
              Welcome to {hoa?.name}
