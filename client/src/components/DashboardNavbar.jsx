198b4af48fd69f4570deeea9fa2e1c1704911a89
jimvelasco <jim.velasco@gmail.com>
2026-04-19 15:44:05 -0600

    Update DashboardNavbar.jsx

--- client/src/components/DashboardNavbar.jsx
+++ client/src/components/DashboardNavbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import NavButton from "./NavButton";
 import ParkingSpacesOverviewNB from "./ParkingSpacesOverviewNB";
-// import hoaLogo from "../assets/HOA7.png";
+import hoaLogo from "../assets/HOA7.png";
+import { Link } from "react-router-dom";
+
 
 
 export default function DashboardNavbar({ title, title2, buttons }) {
@@ -14,13 +16,24 @@ export default function DashboardNavbar({ title, title2, buttons }) {
       <div className="tableview">
         <nav style={{ backgroundColor: "#333", padding: "10px" }}>
           <div className="button-grid">
-            {/* <img
-              src={hoaLogo}
-              alt="HOA Logo"
-              style={{ border:"0px solid white",marginTop:"0px", width: "200px", vAlign:"top",borderRadius: "4px" }}
-            /> */}
+
+            <Link to="/">
+              <img
+                src={hoaLogo}
+                alt="HOA Logo"
+                style={{
+                  border: "0px solid white",
+                  marginTop: "0px",
+                  width: "200px",
+                  verticalAlign: "top",
+                  borderRadius: "4px",
+                  cursor: "pointer" // Recommended for better UX
+                }}
+              />
+            </Link>
+
             {/* <div style={{ color: "#bbb", marginBottom: "0px", fontSize: "18px" }}><b>HOA Parking Solutions</b></div> */}
-  <div style={{ color: "#b9eaa1", marginBottom: "0px", fontSize: "18px" }}><b>HOA Parking Solutions</b></div>
+            {/* <div style={{ color: "#b9eaa1", marginBottom: "0px", fontSize: "18px" }}><b>HOA Parking Solutions</b></div> */}
             <div style={{ color: "white", marginTop: "3px", marginBottom: "0px" }}>{title}</div>
             {/* <div style={{ color: "white", marginTop: "5px", marginBottom: "0px" }}>{title2}</div > */}
             <div style={{ color: "white", marginTop: "3px", marginBottom: "0px" }}> <ParkingSpacesOverviewNB /></div >
@@ -49,13 +62,26 @@ export default function DashboardNavbar({ title, title2, buttons }) {
             color: "#bbb", marginBottom: "5px", alignItems: "center",
             justifyContent: "center", display: "flex", fontSize: "18px"
           }}>
-            <b>HOA Parking Solutions</b>
-             {/* <img
+            <Link to="/">
+              <img
+                src={hoaLogo}
+                alt="HOA Logo"
+                style={{
+                  border: "0px solid white",
+                  marginTop: "0px",
+                  width: "200px",
+                  verticalAlign: "top",
+                  borderRadius: "4px",
+                  cursor: "pointer" // Recommended for better UX
+                }}
+              />
+            </Link>
+            {/* <img
               src={hoaLogo}
               alt="HOA Logo"
               style={{ border:"0px solid white",marginTop:"0px", width: "200px", vAlign:"top",borderRadius: "4px" }}
             /> */}
-            </div>
+          </div>
 
           <div style={{ marginBottom: "5px", color: "white", alignItems: "center", justifyContent: "center", display: "flex" }}>
             {title}
