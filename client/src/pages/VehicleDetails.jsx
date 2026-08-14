a6722a2c5048020553076a6cefd6bf3b9adb8d83
jimvelasco <jim.velasco@gmail.com>
2026-08-14 09:09:00 -0600

    Update VehicleDetails.jsx

--- client/src/pages/VehicleDetails.jsx
+++ client/src/pages/VehicleDetails.jsx
@@ -592,11 +592,18 @@ export default function VehicleDetails() {
                   </select>
                 </div>
 
-                <input
-                  type="hidden"
-                  name="unitnumber"
-                  value={formData.unitnumber}
-                />
+                <div style={{ marginBottom: "0px" }}>
+                  <label className="input-label">
+                    Unit Number *
+                  </label>
+                  <input className="standardinput"
+                    type="text"
+                    name="unitnumber"
+                    value={formData.unitnumber}
+                    onChange={handleFormChange}
+                    required
+                  />
+                </div>
 
 
               </div>
